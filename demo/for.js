2adbc255241c0b057cc0d98cf96dea94b793da48
tedshd <tedshd@gmail.com>
2013-04-21 01:22:46 +0800

    Init repo.

---
+++ demo/for.js
@@ -0,0 +1,13 @@
+/*
+
+ @authors leon <kairyou@qq.com>
+          you
+*/
+var i,
+    sum = 0,
+    console;
+for (i = 0; 100 >= i; i++) {
+    sum = i + sum;
+}
+console.log(sum);
+alert(sum);
---
+++ demo/get.php
@@ -0,0 +1,12 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Get</title>
+</head>
+<body>
+<?pdp
+echo 'Get' . htmlspecialchars($_GET['get_length']);
+?>
+</body>
+</html>
