ff4f58c6807812c2ee253f8909a8f60900e5771e
tedshd <tedshd@gmail.com>
2013-05-14 15:46:11 +0800

    Add index.php to root.

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>PHP</title>
+</head>
+<body>
+    <?php
+        echo "php start";
+    ?>
+    <?php
+        phpinfo();
+    ?>
+</body>
+</html>
