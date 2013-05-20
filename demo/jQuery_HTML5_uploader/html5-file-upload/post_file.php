b9d7f6e731dda2e39ef46342753a658087699131
tedshd <tedshd@gmail.com>
2013-05-20 15:39:15 +0800

    Fix and Add yui_uploader.

--- demo/jQuery_HTML5_uploader/html5-file-upload/post_file.php
+++ demo/jQuery_HTML5_uploader/html5-file-upload/post_file.php
@@ -1,11 +1,11 @@
 <?php
 
-// If you want to ignore the uploaded files, 
+// If you want to ignore the uploaded files,
 // set $demo_mode to true;
 
 $demo_mode = false;
 $upload_dir = 'uploads/';
-$allowed_ext = array('jpg','jpeg','png','gif');
+$allowed_ext = array('jpg','jpeg','png','gif', 'pdf', 'docx', 'pptx', 'xlsx', 'zip');
 
 
 if(strtolower($_SERVER['REQUEST_METHOD']) != 'post'){
@@ -14,31 +14,31 @@ if(strtolower($_SERVER['REQUEST_METHOD']) != 'post'){
 
 
 if(array_key_exists('pic',$_FILES) && $_FILES['pic']['error'] == 0 ){
-	
+
 	$pic = $_FILES['pic'];
 
 	if(!in_array(get_extension($pic['name']),$allowed_ext)){
 		exit_status('Only '.implode(',',$allowed_ext).' files are allowed!');
-	}	
+	}
 
 	if($demo_mode){
-		
+
 		// File uploads are ignored. We only log them.
-		
+
 		$line = implode('		', array( date('r'), $_SERVER['REMOTE_ADDR'], $pic['size'], $pic['name']));
 		file_put_contents('log.txt', $line.PHP_EOL, FILE_APPEND);
-		
+
 		exit_status('Uploads are ignored in demo mode.');
 	}
-	
-	
-	// Move the uploaded file from the temporary 
+
+
+	// Move the uploaded file from the temporary
 	// directory to the uploads folder:
-	
+
 	if(move_uploaded_file($pic['tmp_name'], $upload_dir.$pic['name'])){
 		exit_status('File was uploaded successfuly!');
 	}
-	
+
 }
 
 exit_status('Something went wrong with your upload!');
