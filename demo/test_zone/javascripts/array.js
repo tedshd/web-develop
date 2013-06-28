000aae133ba862e37c463eb44b5db5225cce70ba
tedshd <tedshd@gmail.com>
2013-06-28 18:53:11 +0800

    [Dev|test_zone] Add zone to test code.

---
+++ demo/test_zone/javascripts/array.js
@@ -0,0 +1,82 @@
+/**
+ *
+ * @authors Your Name (you@example.org)
+ * @date    2013-06-27 22:44:00
+ * @version $Id$
+ */
+
+$(function() {
+    var arr = [
+            {
+                a: 1,
+                b: 2,
+                c: 3
+            },
+            {
+                a: 4,
+                b: 5,
+                c: 6
+            },
+            {
+                a: 1,
+                b: 2,
+                c: 3
+            }
+        ],
+        arr_1 = [],
+        tree = [
+            {
+                a: 4,
+                b: 5,
+                c: 6
+            }
+        ],
+        tree_1 = [],
+        arr_n = [1, 10, 20],
+        arr_2 = ['a', 'b', 'c'],
+        arr_3 = ['a'];
+    console.log('arr');
+    console.log(arr);
+    // console.log(arr[0].Object.toString());
+    console.log(arr.length);
+    console.log('tree');
+    console.log(tree);
+    console.log(tree.length);
+    for (var i = 0; arr.length > i; i++) {
+        arr_1.push(jQuery.param(arr[i]));
+    }
+    for (var i = 0; tree.length > i; i++) {
+        tree_1.push(jQuery.param(tree[i]));
+    }
+    console.log(arr_1);
+    console.log(tree_1);
+
+    for (var i = 0; arr_1.length > i; i++) {
+        console.log('loop--');
+        console.log(arr_1[i]);
+        console.log(tree_1.indexOf(arr_1[i]));
+        function newtree(element) {
+            return (element = tree_1[i]);
+        }
+        console.log(newtree());
+        // console.log(arr_1.indexOf(newtree()));
+        var newarr = arr_1.filter(newtree);
+        console.log(newarr);
+    }
+    // console.log(arr_2.indexOf('c'));
+    /**
+     * Array_filter
+     * @param  {[type]} element [description]
+     * @param  {[type]} index   [description]
+     * @param  {[type]} array   [description]
+     * @return {[type]}         [description]
+     */
+    function fun(element, index, value) {
+        return (value = 'a');
+    }
+    var filt = arr_2.filter(fun);
+    console.log('--filter--');
+    console.log(filt);
+
+    console.log();
+});
