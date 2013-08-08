0d6d460bed54d3ee263cb151ebda8df1fc5bf846
tedshd <tedshd@gmail.com>
2013-08-08 23:02:58 +0800

    [Dev|scroll_to_top] Dev to function.

--- demo/jquery_scroll_to_top/javascripts/scroll_to_top.js
+++ demo/jquery_scroll_to_top/javascripts/scroll_to_top.js
@@ -1,14 +1,44 @@
-$(document).ready(function() {
-    $('body').append('<div class="scroll-to-top hidden">Top</div>');
-    var scrollToTop = $('.scroll-to-top');
-    scrollToTop.click(function() {
-        $('body, html').animate({scrollTop: '0px'});
-    });
-    $(window).scroll(function() {
-        if ($(window).scrollTop() > 0) {
-            scrollToTop.removeClass('hidden');
+/*global $, jQuery, alert, console, window*/
+/**
+ * Mockup:
+ *
+ * @authors Ted Shiu (you@example.org)
+ * @date    2013-08-08 20:59:26
+ * @version $Id$
+ */
+
+$(function () {
+    var scrollToTop;
+    scrollToTop = function (set) {
+        var tipNode;
+        if ($(set.node).length) {
+            tipNode = $(set.node);
         } else {
-            scrollToTop.addClass('hidden');
+            $('body').append('<div class="scroll-to-top hidden">Top</div>');
+            tipNode = $('.scroll-to-top');
+            tipNode.css({
+                'position': 'fixed',
+                'bottom': '10px',
+                'right': '10px',
+                'padding': '10px',
+                'cursor': 'pointer',
+                'background': '#ccc'
+            });
         }
-    });
+        tipNode.click(function () {
+            $('body, html').animate(
+                {
+                    scrollTop: '0px'
+                }
+            );
+        });
+        $(window).scroll(function() {
+            if ($(window).scrollTop() > 0) {
+                tipNode.removeClass('hidden');
+            } else {
+                tipNode.addClass('hidden');
+            }
+        });
+    };
+    $.scrollToTop = scrollToTop;
 });
