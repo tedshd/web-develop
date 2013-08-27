1579ffeca21273695a0e05f7c0a2600347fc43de
tedshd <tedshd@gmail.com>
2013-08-27 10:35:04 +0800

    [Study|PHP] File upload.

--- demo/php/get.php
+++ demo/php/get.php
@@ -1,12 +1,3 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>Get</title>
-</head>
-<body>
-<?pdp
-echo 'Get' . htmlspecialchars($_GET['get_length']);
+<?php
+echo 'Get' . $_GET['get_length'];
 ?>
-</body>
-</html>
