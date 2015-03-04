f0efc635238e8a66fda91374a6cef79b1d54cd03
tedshd <tedshd@gmail.com>
2015-03-04 16:33:30 +0800

    [Study] PHP test remainder.

---
+++ demo/php/remainder.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ *
+ * @authors Your Name (you@example.org)
+ * @date    2015-02-02 16:15:56
+ * @version $Id$
+ */
+
+$num = 7;
+echo $num % 2;
+if ($num % 2) {
+    echo 'HIHI';
+}
+
+?>
