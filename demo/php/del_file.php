04dfbb375b73562ea9630f15ec17a1cb5c2db684
tedshd <tedshd@gmail.com>
2015-10-25 13:37:57 +0800

    [dev|php] Modify del file php.

--- demo/php/del_file.php
+++ demo/php/del_file.php
@@ -6,7 +6,7 @@
  * @version $Id$
  */
 
-
+/*
 exec('find ./ .jpg', $output, $return_var);
 
 // var_dump($output);
@@ -17,6 +17,21 @@ foreach ($output as $file) {
     if ($width === 160) {
         exec('rm ' . $file, $output_tmp, $return_var_tmp);
     }
+}
+*/
+
+exec('find ./* -name "*.html"', $output, $return_var);
+
+// var_dump($output);
+
+foreach ($output as $file) {
+    echo $file . "\n";
+    exec('rm ' . $file, $output_tmp, $return_var_tmp);
+    //list($width, $height, $type, $attr) = getimagesize($file);
+    //echo $type . "\n";
+    //if ($type === 'html') {
+        //exec('rm ' . $file, $output_tmp, $return_var_tmp);
+    //}
 }
 
 ?>
