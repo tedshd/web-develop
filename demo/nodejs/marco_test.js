9bef24cd439e95077e54425bec4b3dff09759d1a
tedshd <tedshd@gmail.com>
2017-10-22 20:57:18 +0800

    [dev] Update test marco code.

--- demo/nodejs/marco_test.js
+++ demo/nodejs/marco_test.js
@@ -5,31 +5,100 @@
  * @version $Id$
  */
 
-var a, b, c, d;
+let BufferArray = [];
+let BufferLength = 0;
+const profileCount = 3;
+let profiles = [];
+const marco = 0;
+const itemCount = profileCount;
 
-var BufferArray = [],
-    BufferLength;
-
-a = Buffer.from([224]);
-b = Buffer.from([parseInt(111100, 2)]);
-c = Buffer.from([19]);
-d = Buffer.from([0]);
-
-// var b = new Uint16Array(2);
-// b[0] = 41+1+1+1+0+0;
-// b[1] = 1;
-// b[2] = 1;
-// b[3] = 1;
-// b[4] = 1;
-// b[5] = 1;
-// b[6] = 0;
-// b[7] = 0;
-// b = Buffer.from(b.buffer);
+const title = Buffer.from('CYFI', 'ascii');
+var rev = new Uint16Array(1);
+rev[0] = 0;
+rev = Buffer.from(rev.buffer);
+var itemSize = new Uint16Array(1);
+itemSize[0] = itemCount;
+itemSize = Buffer.from(itemSize.buffer);
+// BufferArray = [title, rev, itemSize];
+// BufferLength = title.length + rev.length + itemSize.length;
 
 
-BufferArray = BufferArray.concat([a, b, c, d]);
+function marcoData(key, action, time) {
+    var keyPage,
+        keyEvent,
+        delayTime;
 
-BufferLength = a.length + b.length + c.length + d.length;
+    if (!key || !action || !time) {
+        console.error('marcoData not set argument');
+        return;
+    }
+    action = parseInt('111' + action, 10);
+    time = time*2;
+
+    keyPage = Buffer.from([key]);
+    keyEvent = Buffer.from([parseInt(action, 2)]);
+    delayTime = new Uint16Array(1);
+    delayTime[0] = time;
+    delayTime = Buffer.from(delayTime.buffer);
+
+    BufferArray = BufferArray.concat([keyPage, keyEvent, delayTime]);
+    BufferLength = BufferLength + keyPage.length + keyEvent.length + delayTime.length;
+}
+
+
+// marcoData(224, '100', 10);
+
+// var keyPage, keyEvent, delayTime;
+
+// keyPage = Buffer.from([224]);
+// keyEvent = Buffer.from([parseInt(111100, 2)]);
+// delayTime = new Uint16Array(1);
+// delayTime[0] = 20;
+// delayTime = Buffer.from(delayTime.buffer);
+
+// BufferArray = BufferArray.concat([keyPage, keyEvent, delayTime]);
+// BufferLength = BufferLength + keyPage.length + keyEvent.length + delayTime.length;
+
+// var e, f, g, h;
+
+// e = Buffer.from([224]);
+// f = Buffer.from([parseInt(111010, 2)]);
+// g = Buffer.from([0]);
+// h = Buffer.from([0]);
+
+// BufferArray = BufferArray.concat([e, f, g, h]);
+// BufferLength = BufferLength + e.length + f.length + g.length + h.length;
+
+function marcoKey(marcoIndex, keycode, type) {
+    type = parseInt('111' + type, 10);
+    var key = Buffer.from([0x18]);
+    var index = new Uint16Array(1);
+    index[0] = parseInt(marcoIndex, 10);
+    index = Buffer.from(index.buffer);
+    var data = Buffer.from([keycode, parseInt(type, 2), 1]);
+    var len = Buffer.from([2]);
+    var arr = [len, key, index, data];
+    BufferArray = BufferArray.concat([len, key, index, data]);
+
+    BufferLength = BufferLength + key.length + index.length + data.length + len.length;
+}
+
+marcoKey(0, 41, '001');
+
+// var key = Buffer.from([0x18]);
+// var index = new Uint16Array(1);
+// index[0] = parseInt(1, 10);
+// index = Buffer.from(index.buffer);
+// var data = Buffer.from([41, parseInt(111001, 2), 1]);
+// // var data = new Uint16Array(2);
+// // data[0] = parseInt(41, 10);
+// // data = Buffer.from(data.buffer);
+// var len = Buffer.from([2]);
+// var arr = [len, key, index, data];
+// BufferArray = BufferArray.concat([len, key, index, data]);
+
+// BufferLength = BufferLength + key.length + index.length + data.length + len.length;
+
 
 
 const buff = Buffer.concat(BufferArray, BufferLength);
